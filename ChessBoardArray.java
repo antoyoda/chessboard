3b6841cfb7b60969318f7a4011edddc5c4f4e141
Antoni Tadych <antoyoda24@gmail.com>
2024-05-28 09:00:08 -0400

    created chessboardarry

--- ChessBoardArray.java
+++ ChessBoardArray.java
@@ -1,11 +1,36 @@
 public class ChessBoardArray {
     private final Piece[][] board = new Piece[8][8];
-
+    private int length = 8;
+    // black pieces
+    // 4 lines of space
+    // white pieces
     public ChessBoardArray() {
-        int length = 8;
+        // black pawns
         for (int i = 0; i < length; i++) {
-            board[1][]
+            board[1][i] = new Pawn(false);
         }
+        // white pawns
+        for (int i = 0; i < length; i++) {
+            board[1][i] = new Pawn(true);
+        }
+        // black pieces
+        board[0][0] = new Rook(false, false); // second false is Boolean hasMoved
+        board[0][7] = new Rook(false, false);
+        board[0][1] = new Knight(false);
+        board[0][6] = new Knight(false);
+        board[0][2] = new Bishop(false);
+        board[0][5] = new Bishop(false);
+        board[0][3] = new Queen(false);
+        board[0][4] = new King(false);
+        // white pieces
+        board[7][0] = new Rook(true, false);
+        board[7][7] = new Rook(true, false);
+        board[7][1] = new Knight(true);
+        board[7][6] = new Knight(true);
+        board[7][2] = new Bishop(true);
+        board[7][5] = new Bishop(true);
+        board[7][3] = new Queen(true);
+        board[7][4] = new King(true);
     }
 
 }
