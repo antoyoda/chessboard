070c9473b90126184ac82db7af7207ab41ad7970
Antoni Tadych <antoyoda24@gmail.com>
2024-05-30 08:47:46 -0400

    Created player and game classes, and logic to create two player objects in the game class. Updated Tester to test it out

--- Game.java
+++ Game.java
@@ -1,10 +1,64 @@
-class Game {
-    // 2 class players
-        // one player will be computer
-    // class board
-        // filled with class pieces
-    // counter for the move
-    // class visuals - updates by the chessboard class
-    // when its your turn:
-        // you can drag pieces and when you stop dragging, the move is made
+import java.util.Scanner;
+
+public class Game {
+  private Player white;
+  private Player black;
+  private Board board = new Board();
+
+  public Game() {
+    white = null;
+    black = null;
+  }
+
+  public String getNameWhite() {
+    if (white != null) {
+      return white.getName();
+    }
+    else {
+      return null;
+    }
+  }
+
+  public String getNameBlack() {
+    if (black != null) {
+      return black.getName();
+    }
+    else {
+      return null;
+    }
+  }
+
+  // initializes player names
+  public void start() {
+    Scanner in = new Scanner(System.in);
+    System.out.print("Welcome, Player 1. You are playing with the white pieces. ");
+    String name1 = "";
+    // check to make sure name isn't empty
+    while (name1.trim().isEmpty()) {
+      System.out.println("What is your name?");
+      name1 = in.nextLine().trim();
+      if (name1.isEmpty()) {
+        System.out.println("Your name can't be empty, try again.");
+      }
+    }
+
+    System.out.print("Welcome, Player 2. You are playing with the black pieces. ");
+    String name2 = "";
+    while (name2.trim().isEmpty()) {
+      System.out.println("What is your name?");
+      name2 = in.nextLine().trim();
+      if (name2.isEmpty()) {
+        System.out.println("Your name can't be empty, try again.");
+      }
+    }
+
+    white = new Player(name1, true);
+    black = new Player(name2, false);
+
+    in.close();
+  }
+  // gets valid chess notation move and makes sure it can be played
+  public void getMove() {
+  }
+
 }
--- Tester.java
+++ Tester.java
@@ -1,6 +1,8 @@
 public class Tester {
   public static void main(String args[]) {
-    Board b = new Board();
-    System.out.println(b);
+    Game game1 = new Game();
+    game1.start();
+    System.out.println("white's name: " + game1.getNameWhite());
+    System.out.println("black's name: " + game1.getNameBlack());
   }
 }
