5fbb048c69bf97668277caae4272323df46ddb0f
Antoni <antoyoda24@gmail.com>
2024-06-04 19:30:32 -0400

    fixed merge issues in Tester.java

--- Tester.java
+++ Tester.java
@@ -1,22 +1,12 @@
 public class Tester {
   public static void main(String args[]) {
-<<<<<<< HEAD
-    Board b = new Board();
-    System.out.println(b);
-    b.movePiece(0, 0, 0, 7);
-    System.out.println(b);
-    System.out.println(b.getPiece(3, 6));
-    System.out.println(b.getPiece(3, 6).isLegalMove(b, 3, 6, 3, 4));
-=======
-    // Board b = new Board();
-    // System.out.println(b);
-    // System.out.println(b.getPiece(1, 6).isLegalMove(b, 1, 6, 1, 5));
-
+    // pawn doesnt work
     Game g = new Game();
+    System.out.println(g.getBoard());
     g.getPlayerNames();
     g.assignActivePlayer();
     g.getMove();
->>>>>>> 3084e83379782f22bd44ecb64f43a4bdaecd4b3b
+    System.out.println(g.getBoard());
   }
 
 }
