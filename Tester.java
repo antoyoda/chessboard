3084e83379782f22bd44ecb64f43a4bdaecd4b3b
Antoni Tadych <antoyoda24@gmail.com>
2024-06-03 23:19:40 -0400

    Started logic for getMove(), untested and running into errors

--- Tester.java
+++ Tester.java
@@ -1,16 +1,12 @@
 public class Tester {
   public static void main(String args[]) {
-    int[] test1 = Game.convertChessNotationToArray("e4");
-    System.out.println("e4 should be 4, 4");
-    System.out.println(test1[0] + " " + test1[1]);
-    int[] test2 = Game.convertChessNotationToArray("g2");
-    System.out.println("g2 should be 6, 6");
-    System.out.println(test2[0] + " " + test2[1]);
-    int[] test3 = Game.convertChessNotationToArray("b3");
-    System.out.println("b3 should be 1, 5");
-    System.out.println(test3[0] + " " + test3[1]);
-    int[] test4 = Game.convertChessNotationToArray("a1");
-    System.out.println("a1 should be 0, 7");
-    System.out.println(test4[0] + " " + test4[1]);
+    // Board b = new Board();
+    // System.out.println(b);
+    // System.out.println(b.getPiece(1, 6).isLegalMove(b, 1, 6, 1, 5));
+
+    Game g = new Game();
+    g.getPlayerNames();
+    g.assignActivePlayer();
+    g.getMove();
   }
 }
