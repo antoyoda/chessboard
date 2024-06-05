f97c10d2aff82ea1af4553ef5e022410f9b8babd
Antoni <antoyoda24@gmail.com>
2024-06-05 16:17:31 -0400

    pawn and rook testing for Tester.jjava

--- Tester.java
+++ Tester.java
@@ -1,13 +1,15 @@
 public class Tester {
   public static void main(String args[]) {
+    // pawn doesnt work
     Game g = new Game();
     g.getPlayerNames();
+    g.assignActivePlayer();
     boolean stop = false;
     while (!stop) {
       System.out.println(g.getBoard());
-      g.assignActivePlayer();
       g.getMove();
       System.out.println(g.getBoard());
+      g.switchTurn();
     }
   }
 }
