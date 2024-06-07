7874fb23cacdd0b178aaff28cefa0bf2351c4d16
Antoni Tadych <antoyoda24@gmail.com>
2024-06-07 08:02:16 -0400

    Fixing Piece.java merge issues

--- Piece.java
+++ Piece.java
@@ -27,37 +27,6 @@ class Pawn extends Piece {
 
     public String toString() {return "P";}
 
-    // public boolean isLegalMove(Board board, int startX, int startY,  int endX, int endY) {
-    //     // add en passant later
-    //     Piece a = board.getPiece(endX, endY);
-    //     if (isWhite()) {
-    //         // move up one
-    //         if ((endY == startY-1) && (endX == startX)) {
-    //             if (a == null) { return true; }
-    //         }
-    //         // move up two
-    //         Piece b = board.getPiece(endX, endY-1);
-    //         if ((startX == 6) && (endY == startY-2) && (endX == startX)) {
-    //             if (a == null && b == null) { return true; }
-    //         }
-    //         // need to check captures
-    //     }
-    //     // black Piece
-    //     else {
-    //         // move down one
-    //         if ((endY == startY+1)  && (endX == startX)) {
-    //             if (a == null) {return true;}
-    //         }
-    //         // move down two
-    //         Piece b = board.getPiece(endX, endY+1);
-    //         if ((startX == 1) && (endY == startY+2) && (endX == startX)) {
-    //             if (a == null && b == null) { return true; }
-    //         }
-    //     }
-    //     return false;
-    //     }
-    // }
-
     public boolean isLegalMove(Board board, int startX, int startY,  int endX, int endY) {
         // add en passant later -- has moved that expires after one move
         if (isWhite()) {
